d34534c674e3594a149aa5fd491f70ea12495cda
liaog_wlhs <liaog@whls.wlwv.k12.or.us>
2024-10-22 09:54:49 -0700

    commiting

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,19 @@
 '''
-Name:
+Name: Guoming Liao
 Date: 10/22/24
 Description: Unit 4 Lesson 1 - while loops
 
 '''
 
+start_number = 10
+while start_number >= 0:
+    print(start_number)
+    start_number = start_number - 1
+print("Blastoff!") # always prints
+
+while True:
+    user_age = int(input("Enter age or -1 to quit: "))
+    if user_age == -1:
+        break
+    else:
+        continue
